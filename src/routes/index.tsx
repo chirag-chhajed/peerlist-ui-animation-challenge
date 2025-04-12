8ed573c2b8fe48790a82cc312e40a8d279f9d0c6
Chirag Chhajed <chhajedchirag18@gmail.com>
2025-04-12 22:14:37 +0530

    Update index.html to add antialiased class, add Radix UI components to package.json, and implement new UI components (Button, Input, Separator, Sheet, Tooltip, Skeleton, Sidebar) with styles. Remove Header component and adjust routes accordingly. Enhance Vite config and styles for better integration.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,39 +1,115 @@
-import { createFileRoute } from '@tanstack/react-router'
-import logo from '../logo.svg'
+import { createFileRoute } from "@tanstack/react-router";
+import { Button, Menu, MenuItem as MenuItemBase } from "react-aria-components";
+import {
+  HomeIcon,
+  MailIcon,
+  MenuIcon,
+  Settings,
+  User,
+  XIcon,
+} from "lucide-react";
+import { motion, AnimatePresence } from "motion/react";
+import { useState } from "react";
 
-export const Route = createFileRoute('/')({
+export const Route = createFileRoute("/")({
   component: App,
-})
+});
+
+const MenuItem = motion(MenuItemBase);
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+  const menuItems = [
+    {
+      icon: HomeIcon,
+    },
+    {
+      icon: MailIcon,
+    },
+    {
+      icon: User,
+    },
+    {
+      icon: Settings,
+    },
+  ];
   return (
-    <div className="text-center">
-      <header className="min-h-screen flex flex-col items-center justify-center bg-[#282c34] text-white text-[calc(10px+2vmin)]">
-        <img
-          src={logo}
-          className="h-[40vmin] pointer-events-none animate-[spin_20s_linear_infinite]"
-          alt="logo"
-        />
-        <p>
-          Edit <code>src/routes/index.tsx</code> and save to reload.
-        </p>
-        <a
-          className="text-[#61dafb] hover:underline"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="h-screen p-8">
+      <div
+        style={{ filter: "url(#gooey-filter-menu)" }}
+        className="flex flex-col   "
+      >
+        <svg
+          aria-label="Gooey filter"
+          width="0"
+          height="0"
+          style={{ position: "absolute" }}
         >
-          Learn React
-        </a>
-        <a
-          className="text-[#61dafb] hover:underline"
-          href="https://tanstack.com"
-          target="_blank"
-          rel="noopener noreferrer"
+          <defs>
+            <filter id="gooey-filter-menu">
+              <feGaussianBlur
+                in="SourceGraphic"
+                stdDeviation="5"
+                result="blur"
+              />
+              <feColorMatrix
+                in="blur"
+                type="matrix"
+                values="1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 18 -7"
+                result="goo"
+              />
+              <feComposite in="SourceGraphic" in2="goo" operator="atop" />
+            </filter>
+          </defs>
+        </svg>
+
+        <Button
+          className="bg-neutral-100 inline-flex items-center justify-center size-12 rounded-full z-10"
+          aria-label="Menu"
+          key={`state-${isOpen}`}
+          onPress={() => setIsOpen(!isOpen)}
         >
-          Learn TanStack
-        </a>
-      </header>
+          {isOpen ? (
+            <motion.div
+              initial={{ filter: "blur(5px)" }}
+              animate={{ filter: "blur(0px)" }}
+            >
+              <XIcon size={24} className="text-zinc-950" />
+            </motion.div>
+          ) : (
+            <motion.div
+              initial={{ filter: "blur(5px)" }}
+              animate={{ filter: "blur(0px)" }}
+            >
+              <MenuIcon size={24} className="text-zinc-950" />
+            </motion.div>
+          )}
+        </Button>
+
+        {isOpen ? (
+          <Menu className={" flex flex-col"}>
+            {menuItems.map((item, index) => (
+              <MenuItem
+                className={
+                  "bg-neutral-100 inline-flex items-center justify-center size-12 rounded-full text-zinc-500 hover:text-zinc-950 hover:cursor-pointer"
+                }
+                initial={{ y: -48 * index }}
+                animate={{ y: 0 }}
+                exit={{ y: -48 * index }}
+                transition={{
+                  type: "spring",
+                  stiffness: 300,
+                  damping: 20,
+                }}
+                id={`menu-item-${index}-${isOpen}`}
+                key={`menu-item-${index}-${isOpen}`}
+              >
+                <item.icon size={24} />
+              </MenuItem>
+            ))}
+          </Menu>
+        ) : null}
+      </div>
     </div>
-  )
+  );
 }
