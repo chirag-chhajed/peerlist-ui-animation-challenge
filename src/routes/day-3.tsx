0f91922f17fbcb26a5c08674494e5419d08f6d62
Chirag Chhajed <chhajedchirag18@gmail.com>
2025-04-15 00:45:19 +0530

    Add checkbox group to RouteComponent: integrated CheckboxGroup and Label for better user interaction. Updated layout and added multiple checkbox options for enhanced functionality.

--- src/routes/day-3.tsx
+++ src/routes/day-3.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { motion, useAnimation } from "motion/react";
-import { Checkbox } from "react-aria-components";
+import { Checkbox, CheckboxGroup, Label } from "react-aria-components";
 import { useEffect, useState } from "react";
 
 export const Route = createFileRoute("/day-3")({
@@ -8,6 +8,19 @@ export const Route = createFileRoute("/day-3")({
 });
 
 function RouteComponent() {
+  return (
+    <div className="h-screen p-8 flex flex-col items-center justify-center">
+      <CheckboxGroup className={"space-y-3"}>
+        <Label className="sr-only">What do you want to do today?</Label>
+        <SingleCheckboxWithLabel label="Buy groceries" />
+        <SingleCheckboxWithLabel label="Contemplate existance" />
+        <SingleCheckboxWithLabel label="Learn SwiftUI" />
+      </CheckboxGroup>
+    </div>
+  );
+}
+
+const SingleCheckboxWithLabel = ({ label }: { label: string }) => {
   const [selected, setSelected] = useState(false);
   const controls = useAnimation();
 
@@ -89,104 +102,102 @@ function RouteComponent() {
   };
 
   return (
-    <div className="h-screen p-8 flex flex-col items-center justify-center">
-      <Checkbox
-        isSelected={selected}
-        onChange={setSelected}
-        className={
-          "group cursor-pointer flex items-center gap-2.5 focus:bg-gray-100 hover:bg-gray-100 p-2 rounded-lg"
-        }
-      >
-        <div className="relative w-[18px] h-[18px]">
-          {/* Background blue circle */}
-          <motion.div
-            className="absolute inset-0 bg-blue-500 rounded-[3.25px]"
-            initial={{ scale: 0 }}
+    <Checkbox
+      isSelected={selected}
+      onChange={setSelected}
+      className={
+        "group cursor-pointer flex items-center gap-2.5 focus:bg-gray-100 hover:bg-gray-100 p-2 rounded-lg"
+      }
+      value={label}
+    >
+      <div className="relative w-[18px] h-[18px]">
+        {/* Background blue circle */}
+        <motion.div
+          className="absolute inset-0 bg-blue-500 rounded"
+          initial={{ scale: 0 }}
+          animate={controls}
+          variants={{
+            bgVisible: variants.bgVisible,
+            bgInvisible: variants.bgInvisible,
+          }}
+        />
+
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="18"
+          height="18"
+          viewBox="0 0 18 18"
+          fill="none"
+          className="absolute inset-0"
+        >
+          <motion.rect
+            x="0.75"
+            y="0.75"
+            width="16.5"
+            height="16.5"
+            rx="3.25"
+            stroke="#D1D5DB"
+            strokeWidth="1.5"
+            className="[stroke-dasharray:60px] rotate-180 origin-center"
+            initial={{ strokeDashoffset: 0 }}
             animate={controls}
             variants={{
-              bgVisible: variants.bgVisible,
-              bgInvisible: variants.bgInvisible,
+              rectInvisible: variants.rectInvisible,
+              rectVisible: variants.rectVisible,
             }}
           />
-
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="18"
-            height="18"
-            viewBox="0 0 18 18"
-            fill="none"
-            className="absolute inset-0"
-          >
-            <motion.rect
-              x="0.75"
-              y="0.75"
-              width="16.5"
-              height="16.5"
-              rx="3.25"
-              stroke="#D1D5DB"
-              strokeWidth="1.5"
-              className="[stroke-dasharray:60px] rotate-180 origin-center"
-              initial={{ strokeDashoffset: 0 }}
-              animate={controls}
-              variants={{
-                rectInvisible: variants.rectInvisible,
-                rectVisible: variants.rectVisible,
-              }}
-            />
-            <motion.path
-              d="M5 9L8 12L13 6"
-              stroke="#fff"
-              strokeWidth="1.5"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="[stroke-dasharray:15px]"
-              initial={{ strokeDashoffset: 15 }}
-              animate={controls}
-              variants={{
-                checkVisible: variants.checkVisible,
-                checkInvisible: variants.checkInvisible,
-              }}
-            />
-          </svg>
-        </div>
-
-        <div className="relative">
-          <motion.span
-            className="text-zinc-950"
+          <motion.path
+            d="M5 9L8 12L13 6"
+            stroke="#fff"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="[stroke-dasharray:15px]"
+            initial={{ strokeDashoffset: 15 }}
             animate={controls}
             variants={{
-              lineVisible: variants.textSelected,
-              lineInvisible: variants.textNormal,
+              checkVisible: variants.checkVisible,
+              checkInvisible: variants.checkInvisible,
             }}
-          >
-            Contenmplate Existence
-          </motion.span>
-          <svg
-            className="absolute inset-0 w-full h-full pointer-events-none"
-            viewBox="0 0 100 20"
-            preserveAspectRatio="none"
-          >
-            <motion.path
-              d="M0,10 L100,10"
-              stroke="#9f9fa9"
-              strokeWidth="1"
-              strokeLinecap="round"
-              fill="none"
-              className="[stroke-dasharray:100]"
-              initial={{ strokeDashoffset: 100 }}
-              animate={controls}
-              variants={{
-                lineVisible: variants.lineVisible,
-                lineInvisible: variants.lineInvisible,
-              }}
-            />
-          </svg>
-        </div>
-      </Checkbox>
-    </div>
-  );
-}
+          />
+        </svg>
+      </div>
 
+      <div className="relative">
+        <motion.span
+          className="text-zinc-950"
+          animate={controls}
+          variants={{
+            lineVisible: variants.textSelected,
+            lineInvisible: variants.textNormal,
+          }}
+        >
+          {label}
+        </motion.span>
+        <svg
+          className="absolute inset-0 w-full h-full pointer-events-none"
+          viewBox="0 0 100 20"
+          preserveAspectRatio="none"
+        >
+          <motion.path
+            d="M0,10 L100,10"
+            stroke="#9f9fa9"
+            strokeWidth="1"
+            strokeLinecap="round"
+            fill="none"
+            className="[stroke-dasharray:100]"
+            initial={{ strokeDashoffset: 100 }}
+            animate={controls}
+            variants={{
+              lineVisible: variants.lineVisible,
+              lineInvisible: variants.lineInvisible,
+            }}
+          />
+        </svg>
+      </div>
+    </Checkbox>
+  );
+};
 // When the checkbox is selected:
 // 1. the checkbox rect path becomes invisible
 // 2. the bg blue pops up or scales up to 1
