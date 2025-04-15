cd6648984374d9270575f8d06e1f8f4302d0c7ac
Chirag Chhajed <chhajedchirag18@gmail.com>
2025-04-16 00:23:53 +0530

    Add day-4 styles and implement tab functionality in RouteComponent. Introduced CSS for layout and button styles, and added state management for tab switching with clip-path animations.

--- src/routes/day-4.tsx
+++ src/routes/day-4.tsx
@@ -1,9 +1,81 @@
 import { createFileRoute } from "@tanstack/react-router";
-
+import { useEffect, useRef, useState } from "react";
+import "./day-4.css";
 export const Route = createFileRoute("/day-4")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <div>Coming Soon</div>;
+  const [activeTab, setActiveTab] = useState(TABS[0].name);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const activeTabElementRef = useRef<HTMLButtonElement>(null);
+
+  useEffect(() => {
+    const container = containerRef.current;
+
+    if (activeTab && container) {
+      const activeTabElement = activeTabElementRef.current;
+
+      if (activeTabElement) {
+        const { offsetLeft, offsetWidth } = activeTabElement;
+
+        const clipLeft = offsetLeft;
+        const clipRight = offsetLeft + offsetWidth;
+        container.style.clipPath = `inset(0 ${Number(
+          100 - (clipRight / container.offsetWidth) * 100
+        ).toFixed()}% 0 ${Number(
+          (clipLeft / container.offsetWidth) * 100
+        ).toFixed()}% round 17px)`;
+      }
+    }
+  }, [activeTab, activeTabElementRef, containerRef]);
+
+  return (
+    <div className="wrapper">
+      <ul className="list">
+        {TABS.map((tab) => (
+          <li key={tab.name}>
+            <button
+              ref={activeTab === tab.name ? activeTabElementRef : null}
+              data-tab={tab.name}
+              onClick={() => {
+                setActiveTab(tab.name);
+              }}
+              className="button"
+            >
+              {tab.name}
+            </button>
+          </li>
+        ))}
+      </ul>
+
+      <div aria-hidden className="clip-path-container" ref={containerRef}>
+        <ul className="list list-overlay">
+          {TABS.map((tab) => (
+            <li key={tab.name}>
+              <button
+                data-tab={tab.name}
+                onClick={() => {
+                  setActiveTab(tab.name);
+                }}
+                className="button-overlay button"
+                tabIndex={-1}
+              >
+                {tab.name}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
+
+const TABS = [
+  {
+    name: "Payments",
+  },
+  {
+    name: "Balances",
+  },
+];
