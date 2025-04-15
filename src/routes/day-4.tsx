d77a927946870aed07f55b3b0f868142c8b834f7
Chirag Chhajed <chhajedchirag18@gmail.com>
2025-04-16 01:26:01 +0530

    Refactor RouteComponent: replace tab functionality with pricing options, update layout for better UX, and change background color in CSS. Simplified state management and adjusted clip-path for smoother transitions.

--- src/routes/day-4.tsx
+++ src/routes/day-4.tsx
@@ -1,81 +1,84 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { useEffect, useRef, useState } from "react";
-import "./day-4.css";
 export const Route = createFileRoute("/day-4")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  const [activeTab, setActiveTab] = useState(TABS[0].name);
-  const containerRef = useRef<HTMLDivElement>(null);
-  const activeTabElementRef = useRef<HTMLButtonElement>(null);
+  const [activePricing, setActivePricing] = useState("Free");
+  const pricingContainerRef = useRef<HTMLDivElement>(null);
+  const activePricingRef = useRef<HTMLLIElement>(null);
 
   useEffect(() => {
-    const container = containerRef.current;
+    const container = pricingContainerRef.current;
+    const activeElement = activePricingRef.current;
 
-    if (activeTab && container) {
-      const activeTabElement = activeTabElementRef.current;
+    if (container && activeElement) {
+      const { offsetLeft, offsetWidth } = activeElement;
+      const clipLeft = offsetLeft;
+      const clipRight = offsetLeft + offsetWidth;
 
-      if (activeTabElement) {
-        const { offsetLeft, offsetWidth } = activeTabElement;
-
-        const clipLeft = offsetLeft;
-        const clipRight = offsetLeft + offsetWidth;
-        container.style.clipPath = `inset(0 ${Number(
-          100 - (clipRight / container.offsetWidth) * 100
-        ).toFixed()}% 0 ${Number(
-          (clipLeft / container.offsetWidth) * 100
-        ).toFixed()}% round 17px)`;
-      }
+      container.style.clipPath = `inset(0 ${Number(
+        100 - (clipRight / container.offsetWidth) * 100
+      ).toFixed()}% 0 ${Number(
+        (clipLeft / container.offsetWidth) * 100
+      ).toFixed()}% round 999px)`;
     }
-  }, [activeTab, activeTabElementRef, containerRef]);
+  }, [activePricing]);
 
   return (
-    <div className="wrapper">
-      <ul className="list">
-        {TABS.map((tab) => (
-          <li key={tab.name}>
-            <button
-              ref={activeTab === tab.name ? activeTabElementRef : null}
-              data-tab={tab.name}
-              onClick={() => {
-                setActiveTab(tab.name);
-              }}
-              className="button"
+    <div className="flex flex-col items-center justify-center min-h-screen gap-8">
+      <div className="flex items-center justify-center p-1 shadow-md rounded-full">
+        <div className="rounded-full font-semibold relative">
+          <ul className="flex gap-2 rounded-full">
+            <li
+              ref={activePricing === "Free" ? activePricingRef : null}
+              className="w-52 py-2 flex items-center justify-center rounded-full cursor-pointer"
+              onClick={() => setActivePricing("Free")}
             >
-              {tab.name}
-            </button>
-          </li>
-        ))}
-      </ul>
-
-      <div aria-hidden className="clip-path-container" ref={containerRef}>
-        <ul className="list list-overlay">
-          {TABS.map((tab) => (
-            <li key={tab.name}>
-              <button
-                data-tab={tab.name}
-                onClick={() => {
-                  setActiveTab(tab.name);
-                }}
-                className="button-overlay button"
-                tabIndex={-1}
-              >
-                {tab.name}
-              </button>
+              <div>
+                <p>Free</p>
+              </div>
             </li>
-          ))}
-        </ul>
+            <li
+              ref={activePricing === "Premium" ? activePricingRef : null}
+              className="w-52 py-2 flex items-center justify-center rounded-full cursor-pointer"
+              onClick={() => setActivePricing("Premium")}
+            >
+              <div className="flex flex-col items-center justify-center">
+                <p>Premium</p>
+                <p className="text-sm font-medium flex items-center gap-1">
+                  <span>Monthly</span>
+                  <span>.</span>
+                  <span>Annual</span>
+                </p>
+              </div>
+            </li>
+          </ul>
+          <div
+            ref={pricingContainerRef}
+            className="w-full absolute overflow-hidden z-10 inset-0 transition-[clip-path] duration-300 ease-in-out bg-black"
+          >
+            <ul className="flex gap-2 rounded-full bg-black">
+              <li className="w-52 py-2 flex items-center justify-center bg-black text-white rounded-full">
+                <div>
+                  <p>Free</p>
+                </div>
+              </li>
+              <li className="w-52 py-2 flex items-center justify-center bg-black text-white rounded-full">
+                <div className="flex flex-col items-center justify-center">
+                  <p>Premium</p>
+                  <p className="text-sm font-medium flex items-center gap-1">
+                    <span>Monthly</span>
+                    <span>.</span>
+                    <span>Annual</span>
+                  </p>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
 }
-
-const TABS = [
-  {
-    name: "Payments",
-  },
-  {
-    name: "Balances",
-  },
-];
